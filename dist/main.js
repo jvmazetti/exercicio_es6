ee974f2d3a455f5f3cd196d8d8251ffabf2199aa
João Victor Mazetti Borges <jvmazettib@gmail.com>
2023-11-14 17:01:51 -0300

    commit inicial

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function Aluno(nome, nota) {
+  this.nome = nome;
+  this.nota = nota;
+  this.aprovado = function () {
+    if (nota >= 6) {
+      alunoAprovado += Aluno;
+      console.log(nome);
+    }
+  };
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+function Aluno(nome, nota){
+    this.nome = nome;
+    this.nota = nota;
+    this.aprovado = function(){
+		if (nota >= 6){
+            alunoAprovado += Aluno
+            console.log(nome)
+        }     
+	}
+}
